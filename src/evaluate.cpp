fa106b28f3743a3c97ab02b4222b93393e9d864d
Fabian Fichter <ianfab@users.noreply.github.com>
2020-05-17 22:36:35 +0200

    scale factor

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1060,8 +1060,8 @@ namespace {
         if (   pos.opposite_bishops()
             && pos.non_pawn_material() == 2 * BishopValueMg)
             sf = 22 ;
-        else
-            sf = std::min(sf, 36 + (pos.opposite_bishops() ? 2 : 7) * (pos.count<PAWN>(strongSide) + pos.count<SOLDIER>(strongSide)));
+        else if (pos.king_type() != WAZIR)
+            sf = std::min(sf, 36 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide));
 
         sf = std::max(0, sf - (pos.rule50_count() - 12) / 4);
     }
